cfe956ff9ec0ee500080c10fca0f12f86c2e5a60
edgarvng <100292703+edgarvng@users.noreply.github.com>
2023-08-30 13:00:41 -0400

    Actividad1

---
+++ DIa3/Actividad1.py
@@ -0,0 +1,18 @@
+# Actividad 1
+# Edgar Venegas
+
+# Codigo para invertir cadena.
+string = "Edgar"
+print(string[::-1])
+
+#Palindromo
+
+print("Usa solo minusculas.")
+x = input("Dime la palabra:")
+y = (x[::-1])
+
+if (x == y):
+    print ("Es palindromo")
+else:
+    print("No es palindromo.")
+  
